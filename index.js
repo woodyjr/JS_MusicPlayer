56302c875179b363c4d6231ca763071d078789ff
woodyjr <woodyjr@mail.uc.edu>
2017-06-14 15:15:10 -0400

    load album cover, load songs, onclick

--- index.js
+++ index.js
@@ -13,12 +13,31 @@ const URL = 'https://uc-music-service.herokuapp.com';
         albums.forEach(function(album){
             const li = document.createElement('li')
 
-            li.innerText = album.name
+            //li.innerText = album.name
+
+            var albumCover = document.createElement("IMG");
+            albumCover.setAttribute("src", album.cover);
+            albumCover.setAttribute("width", "150");
+            albumCover.setAttribute("height", "150");
+            li.appendChild(albumCover);
 
             li.onclick = async function () {
                 try{
                     const response = await fetch(`${URL}/album/${album.id}`)
                     const albumDetails = await response.json();
+                    var songsList = document.createElement("ul");
+
+                    document.getElementById("songs").innerHTML = "";
+                    const songList = document.getElementById('songs')
+
+                    let s = document.createElement('ul');
+                    for(let i=0; i < albumDetails.songs.length; i++)
+                    {
+                        let d = document.createElement('li');
+                        d.innerText = albumDetails.songs[i].name;
+                        s.appendChild(d);
+                        songList.appendChild(s);
+                    }
 
                     console.log(albumDetails);
                 }
@@ -27,6 +46,7 @@ const URL = 'https://uc-music-service.herokuapp.com';
                 }
             };
             albumList.appendChild(li);
+            
         })
 
     }
